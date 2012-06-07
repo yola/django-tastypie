2ccd5c9ba6d710a2fea08eade5dd1bfce621cca4
Michael Joseph <michaeljoseph@gmail.com>
2012-06-07 09:12:36 +0200

    Correct python-dateutil package name

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     ],
     install_requires=[
         'mimeparse',
-        'python_dateutil >= 1.5, < 2.0',
+        'python-dateutil >= 1.5, < 2.0',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
